c676b026f93e7070df6a7d7803c31abaf7efbbf9
David <70871401+lordaval@users.noreply.github.com>
2023-08-08 12:21:53 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 const main_input = document.querySelector("#main_input");
 const to_do_list = document.querySelector(".to-do-list");
-const item_list = document.querySelectorAll(".list_item");
 
 document.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
@@ -8,24 +7,15 @@ document.addEventListener("keypress", (e) => {
     }
 });
 
-item_list.forEach((item) => {
-    item.addEventListener("mouseenter", () => {
-        generateListButtons(item);
-    });
-    item.addEventListener("mouseleave", () => {
-        removeDeleteButton(item);
-    });
-});
-
 function generateListButtons(item) {
     if (!item.querySelector(".delete_button")) {
         const delete_button = document.createElement("button");
         delete_button.setAttribute("class", "delete_button");
-        
+
         const garbage_icon = document.createElement("img");
         garbage_icon.setAttribute("src", "./assets/trash.svg");
         delete_button.appendChild(garbage_icon);
-        
+
         delete_button.addEventListener("click", () => {
             removeItem(item);
         });
@@ -44,11 +34,11 @@ function removeDeleteButton(item) {
 function insertItem() {
     const value = main_input.value.trim();
     if (value === "") {
-        alert("Não é possível inserir um item vazio");
+        alert("Não é possível inserir uma tarefa vazia");
     } else {
         const item_list = document.createElement("div");
         item_list.setAttribute("class", "list_item");
-        
+
         const h1 = document.createElement("h1");
         h1.textContent = value;
         item_list.appendChild(h1);
@@ -88,13 +78,12 @@ function getItemsFromLocalStorage() {
     return items;
 }
 
-// Carregar os itens do localStorage ao carregar a página
 window.addEventListener("DOMContentLoaded", () => {
     const items = getItemsFromLocalStorage();
     items.forEach((item) => {
         const itemElement = document.createElement("div");
         itemElement.setAttribute("class", "list_item");
-        
+
         const h1 = document.createElement("h1");
         h1.textContent = item;
         itemElement.appendChild(h1);
